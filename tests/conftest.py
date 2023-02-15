60baf368683aee78606b1a391dd54f2b844ff321
michaldanielewicz <michaldanielewicz94@gmail.com>
2023-02-15 15:41:44 +0100

    move fixture from conftest

--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-
-@pytest.fixture
-def raw_input():
-    return "0 0 0 0 0 0 0 0 0\n0 1 0 0 0 0 0 0 0\n1 1 1 0 0 0 1 0 0\n1 1 0 0 0 1 1 1 0\n0 0 0 0 0 1 1 0 0\n0 0 1 0 0 0 0 0 0\n1 1 0 0 0 0 0 0 0\n0 0 0 0 0 1 1 0 0\n"
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,11 @@
-from main import TerrainMapConverter
+import pytest
+
+from main import IslandCounter, TerrainMapConverter
+
+
+@pytest.fixture
+def raw_input():
+    return "0 0 0 0 0 0 0 0 0\n0 1 0 0 0 0 0 0 0\n1 1 1 0 0 0 1 0 0\n1 1 0 0 0 1 1 1 0\n0 0 0 0 0 1 1 0 0\n0 0 1 0 0 0 0 0 0\n1 1 0 0 0 0 0 0 0\n0 0 0 0 0 1 1 0 0\n"
 
 
 def test_get_terrain_map(raw_input):
